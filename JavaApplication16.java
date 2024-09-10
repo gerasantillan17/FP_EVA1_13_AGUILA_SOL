88f0acaa935b78a816d86079f5c103720d4a8301
gerasantillan17 <gerapumas122@gmail.com>
2024-09-10 16:43:08 -0600

    Add files via upload

---
+++ JavaApplication16.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication16;
+
+/**
+ *
+ * @author gerar
+ */
+public class JavaApplication16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double valor = Math.random();
+        if(valor <0.5){
+        System.out.println("AGUILA");
+        }else{
+            System.out.println("Sol");
+        }
+    }
+    
+}
